686e074d98765b6f42613177e54f6dc7a48371cd
clawdyvan <clawdyvan@hotmail.com>
2015-05-30 23:35:51 -0300

    Repsitório criado
    Repsitório criado

---
+++ DengueApp/DengueApp.WindowsPhone/AtividadesPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace DengueApp
+{
+    public sealed partial class AtividadesPage : Page
+    {
+
+        public List<String> ListItems { get; set; }
+
+        public AtividadesPage()
+        {
+            this.InitializeComponent();
+
+            ListItems = new List<string>();
+
+            ListItems.Add("Item1");
+            ListItems.Add("Item2");
+            ListItems.Add("Item3");
+            ListItems.Add("Item4");
+            ListItems.Add("Item5");
+            ListItems.Add("Item6");
+
+            lvAtividades.ItemsSource = ListItems;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
---
+++ DengueApp/DengueApp.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Phone.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace DengueApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+
+        private void HubSectionLoaded(object sender, RoutedEventArgs e)
+        {
+            var grid = (Grid)sender;
+            var page = this.GetPageForSection(grid.Name);
+
+            grid.Children.Add(page);
+        }
+
+        private Page GetPageForSection(String sectionName)
+        {
+
+            switch (sectionName)
+            {
+                case "section1":
+                    return new HomePage();
+
+                case "section2":
+                    return new AtividadesPage();
+
+                default:
+                    return null;
+            }
+        }
+
+        void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                e.Handled = true;
+                Frame.GoBack();
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+    }
+}
