0b1b928563f068fb3328f55082c1ad6f35d18f1c
clawdyvan <clawdyvan@hotmail.com>
2015-05-31 19:31:50 -0300

    Classe NavgationUtils criada

---
+++ DengueApp/DengueApp.WindowsPhone/Model/NavigationUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DengueApp.Model
+{
+    public class NavigationUtils
+    {
+
+        public static Frame getFrame()
+        {
+            return Window.Current.Content as Frame;
+        }
+        
+    }
+}
