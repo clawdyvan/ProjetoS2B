1146185820c38560fc62a56392679f2c01b7ffac
clawdyvan <clawdyvan@hotmail.com>
2015-05-31 18:34:45 -0300

    HomePage - Alterações de detalhes e evento de click em botões

--- DengueApp/DengueApp.WindowsPhone/HomePage.xaml.cs
+++ DengueApp/DengueApp.WindowsPhone/HomePage.xaml.cs
@@ -27,7 +27,7 @@ namespace DengueApp
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void MenuHomeButtonClick(object sender, RoutedEventArgs e)
         {
             var frame = Window.Current.Content as Frame;
             frame.Navigate(typeof(FuncionalidadesSecundariasPage));
