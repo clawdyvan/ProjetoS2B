6ef5b687206fed9b54ed434011231343d76475cc
viniciustads <vinicius_venancio2007@hotmail.com>
2015-05-31 18:55:04 -0300

    windows app style brancos

---
+++ DengueApp/DengueApp.Windows/Model/ItemListaAtividades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DengueApp.Model
+{
+    public class ItemListaAtividades
+    {
+        public string Id { get; set; }
+        public string Titulo { get; set; }
+        public string Subtitulo { get; set; }
+        public bool AtividadeConcluida { get; set; }
+    }
+}
