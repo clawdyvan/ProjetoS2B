243b42a41d5bfc24733dd228bf0406983a635860
Ari03 <ari.livre@gmail.com>
2015-06-04 22:13:44 -0300

    AdicionandoPastaImagemTelaDengue

---
+++ DengueApp/DengueApp.WindowsPhone/OqeDenguePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+
+namespace DengueApp
+{
+    
+    public sealed partial class OqeDenguePage : Page
+    {
+        public OqeDenguePage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}
