af686a1279113068fc5c077e8448904b663102f8
Ari03 <ari.livre@gmail.com>
2015-06-05 21:31:05 -0300

    Add paginas secundarias todas

--- DengueApp/DengueApp.WindowsPhone/ContraIndicacoesPage.xaml.cs
+++ DengueApp/DengueApp.WindowsPhone/ContraIndicacoesPage.xaml.cs
@@ -13,21 +13,27 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace DengueApp
 {
-    
-    public sealed partial class OqeDenguePage : Page
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ContraIndicacoesPage : Page
     {
-        public OqeDenguePage()
+        public ContraIndicacoesPage()
         {
             this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
         }
-
-      }
+    }
 }
