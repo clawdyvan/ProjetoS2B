01b5812dc1c68c310e29aa2e2dab235f02ba8d4a
Ari03 <ari.livre@gmail.com>
2015-06-06 19:23:29 -0300

    Add telas para windows

--- DengueApp/DengueApp.Windows/SintomasPage.xaml.cs
+++ DengueApp/DengueApp.Windows/SintomasPage.xaml.cs
@@ -13,31 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace DengueApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HomePage : Page
+    public sealed partial class SintomasPage : Page
     {
-        public HomePage()
+        public SintomasPage()
         {
             this.InitializeComponent();
-        }
-
-        private void WDButton_Click(object sender, RoutedEventArgs e)
-        {
-            var frame = Window.Current.Content as Frame;
-            frame.Navigate(typeof(FuncionalidadesSecundariasPage));
-        }
-        
-        
-        
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
         }
     }
 }
